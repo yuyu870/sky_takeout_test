9dbf493bb6e977a791eca42949039eebf98c4b5a
shan <1524787418@qq.com>
2025-12-09 20:41:44 +0800

    第八天

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -20,4 +20,12 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+    /**
+     * 根据id查询
+     * @param userId
+     * @return
+     */
+    @Select("SELECT * from user where id=#{id}")
+    User getById(Long userId);
 }
